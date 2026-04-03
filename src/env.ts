99d4d699187c70b4567e69b5ed6b8268ac2ed31a
michaelchristwin <kelechukwuchristwin@gmail.com>
2026-04-03 08:07:09 +0100

    Chore: Made AUTH_TOKEN optional

--- src/env.ts
+++ src/env.ts
@@ -3,21 +3,12 @@ import { config } from "dotenv";
 
 config();
 
-const EnvSchema = z
-  .object({
-    NODE_ENV: z.string().default("development"),
-    DATABASE_URL: z.url(),
-    DATABASE_AUTH_TOKEN: z.string().optional(),
-    PORT: z.coerce.number().default(3000),
-  })
-  .superRefine((input, ctx) => {
-    if (input.NODE_ENV === "production" && !input.DATABASE_AUTH_TOKEN) {
-      ctx.addIssue({
-        code: "custom",
-        message: "Must be set when NODE_ENV is production",
-      });
-    }
-  });
+const EnvSchema = z.object({
+  NODE_ENV: z.string().default("development"),
+  DATABASE_URL: z.url(),
+  DATABASE_AUTH_TOKEN: z.string().optional(),
+  PORT: z.coerce.number().default(3000),
+});
 
 const env = EnvSchema.parse(process.env);
 
